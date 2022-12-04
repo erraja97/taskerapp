c1048bf5a524f1f6a73c2d58f25823faeb8f71b9
Raja Gupta <erraja97@gmail.com>
2022-12-04 13:58:28 +0530

    listen to PORT

---
+++ backend/server.js
@@ -0,0 +1,7 @@
+//import app.js
+const app = require("./app");
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`App is listening at http://localhost:${PORT}`);
+});
