e221778c25247efade2a33490f85bb5ae5d8ed5c
Raja Gupta <erraja97@gmail.com>
2022-12-04 14:53:12 +0530

    Imports mongoose and destructure schema & model variables

---
+++ backend/models/TaskerSchema.js
@@ -0,0 +1,10 @@
+//importing mongoose
+const mongoose = require("mongoose");
+/**
+ * Destructing following from Mongoose
+ * a) Schema Constructor - creates schema for database
+ * b) model method - to convert schema into model
+ */
+const { Schema, model } = mongoose;
+
+
