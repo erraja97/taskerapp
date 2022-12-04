8db15de0526bd878c90d01b2fb9fe0727a094ad3
Raja Gupta <erraja97@gmail.com>
2022-12-04 14:12:28 +0530

    home router added

--- backend/routes/TaskerRouter.js
+++ backend/routes/TaskerRouter.js
@@ -1,5 +1,10 @@
-//import express
+//import express and router
 const express = require("express");
 const router = express.Router();
+//importing controller.js
+const { home } = require("../controllers/TaskerControllers");
+
+//creating home route
+router.get("/", home);
 
 module.exports = router;
