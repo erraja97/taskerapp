05df036b979a43dd2bf85f6f83f46eb8d51cc311
Raja Gupta <erraja97@gmail.com>
2022-12-04 15:18:53 +0530

    Tasker Schema added

--- backend/models/TaskerSchema.js
+++ backend/models/TaskerSchema.js
@@ -7,4 +7,20 @@ const mongoose = require("mongoose");
  */
 const { Schema, model } = mongoose;
 
+const TaskerSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Title is Required"],
+      maxLength: [30, "Maximum length is 30 Character"],
+    },
+    tasks: {
+      type: [{ type: String }],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
+module.exports = model("tasker", TaskerSchema);
